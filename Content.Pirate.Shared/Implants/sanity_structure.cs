3cd3d0006d5e055afaf489d61ea6cd736b220ab1
Open The World <320017039+111openyourmind111@users.noreply.github.com>
2026-08-22 22:21:16 -0400

    Update sanity_structure.cs

--- Content.Pirate.Shared/Implants/sanity_structure.cs
+++ Content.Pirate.Shared/Implants/sanity_structure.cs
@@ -1,174 +1,63 @@
 using Robust.Shared.GameObjects;
-
-
 using Robust.Shared.Serialization;
-
-
-
-
-
 namespace Content.Pirate.Shared.Implants.Cyberpsychosis;
 
-
-
-
-
-public enum SanityState : byte
-
-
-{
-
-
+public enum SanityState : byte {
     Normal = 0,
-
-
     LessNormal = 1,
-
-
     CloseCyberpsychosis = 2,
-
-
     Cyberpsychosis = 3,
-
-
 }
 
 
 [RegisterComponent]
-
-
-public sealed partial class CyberpsychosisComponent : Component
-
-
-{
-
-
-
-
+public sealed partial class CyberpsychosisComponent : Component {
 
     [DataField("sanityValue")]
-
-
     public int SanityValue { get; set; } = 100;
 
 
-
-
-
-
-
-
     [DataField("sanityState")]
-
-
     public SanityState CurrentState { get; set; } = SanityState.Normal;
 
 
-
-
-
-
-
-
     [DataField("decreaseSanity")]
-
-
     public float DecreaseSanity { get; set; } = 0.1f;
 
 
-
-
-
     [DataField("autoActionThreshold")]
-
-
     public int AutoActionThreshold { get; set; } = 20;
 
 
-
-
-
-
-
-
     [DataField("cyberpsychosisThreshold")]
-
-
     public int CyberpsychosisThreshold { get; set; } = 0;
 
 
-
-
-
     [DataField("recoverySanity")]
-
-
     public float RecoverySanity { get; set; } = 0.05f;
 
 
-
-
-
     [DataField("antidepressantBonus")]
-
-
     public float AntidepressantBonus { get; set; } = 15f;
 
 
-
-
-
     [DataField("drugRecoveryBonus")]
-
-
     public float DrugRecoveryBonus { get; set; } = 10f;
 
 
-
-
-
     [DataField("isCyberpsychotic")]
-
-
     public bool IsCyberpsychotic { get; set; } = false;
 
 
-
-
-
     [DataField("activeImplantCount")]
-
-
     public int ActiveImplantCount { get; set; } = 0;
 
 
-
-
-
-
-
-
     public float UncontrolledActionTimer { get; set; } = 0f;
-
-
-
-
-
-
-
-
     [DataField("uncontrolledActionCheckInterval")]
 
 
     public float UncontrolledActionCheckInterval { get; set; } = 1f;
-
-
-
-
-
-
-
-
     [DataField("uncontrolledActionChance")]
 
 
@@ -178,28 +67,9 @@ public sealed partial class CyberpsychosisComponent : Component
 }
 
 
-
-
-
-
-
-
 [Serializable, NetSerializable]
-
-
-public sealed class CyberpsychosisStateMessage : ComponentMessage
-
-
-{
-
-
+public sealed class CyberpsychosisStateMessage : ComponentMessage {
     public int SanityValue { get; set; }
-
-
     public SanityState CurrentState { get; set; }
-
-
     public bool IsCyberpsychotic { get; set; }
-
-
 }
