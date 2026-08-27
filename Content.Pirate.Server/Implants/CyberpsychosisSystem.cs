c85813337e108b4b239cf64dd63d37544eb3041c
Open The World <320017039+111openyourmind111@users.noreply.github.com>
2026-08-27 18:35:39 -0400

    Update CyberpsychosisSystem.cs

--- Content.Pirate.Server/Implants/CyberpsychosisSystem.cs
+++ Content.Pirate.Server/Implants/CyberpsychosisSystem.cs
@@ -1,6 +1,8 @@
 using Content.Pirate.Shared.Implants.Cyberpsychosis;
 using Content.Shared.Alert;
 using Content.Shared.GameTicking;
+using Content.Shared.Implants;
+using Content.Shared.Implants.Components;
 using Robust.Shared.GameObjects;
 
 namespace Content.Pirate.Server.Implants;
@@ -14,20 +16,41 @@ public sealed class CyberpsychosisSystem : EntitySystem
         base.Initialize();
 
         SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawnComplete);
+        SubscribeLocalEvent<CyberpsychosisComponent, ComponentStartup>(OnStartup);
+        SubscribeLocalEvent<CyberpsychosisLoadComponent, ImplantImplantedEvent>(OnImplantAdded);
+        SubscribeLocalEvent<CyberpsychosisLoadComponent, ImplantRemovedEvent>(OnImplantRemoved);
         SubscribeLocalEvent<CyberpsychosisComponent, ComponentShutdown>(OnShutdown);
     }
 
     private void OnPlayerSpawnComplete(PlayerSpawnCompleteEvent args)
     {
-        var mob = args.Mob;
-
-        var component = EnsureComp<CyberpsychosisComponent>(mob);
-        RefreshAlert(mob, component);
+        var component = EnsureComp<CyberpsychosisComponent>(args.Mob);
+        RecalculateSanity(args.Mob, component);
 
         Log.Info(
-            $"[CYBERPSYCHOSIS] Added to {ToPrettyString(mob)}. " +
-            $"Sanity = {component.SanityValue}, " +
-            $"State = {component.CurrentState}");
+            $"[CYBERPSYCHOSIS] Added to {ToPrettyString(args.Mob)}. " +
+            $"Sanity = {component.SanityValue}, State = {component.CurrentState}");
+    }
+
+    private void OnStartup(Entity<CyberpsychosisComponent> ent, ref ComponentStartup args)
+    {
+        RecalculateSanity(ent, ent.Comp);
+    }
+
+    private void OnImplantAdded(Entity<CyberpsychosisLoadComponent> ent, ref ImplantImplantedEvent args)
+    {
+        if (!TryComp<CyberpsychosisComponent>(args.Implanted, out var cyber))
+            return;
+
+        RecalculateSanity(args.Implanted, cyber);
+    }
+
+    private void OnImplantRemoved(Entity<CyberpsychosisLoadComponent> ent, ref ImplantRemovedEvent args)
+    {
+        if (!TryComp<CyberpsychosisComponent>(args.Implanted, out var cyber))
+            return;
+
+        RecalculateSanity(args.Implanted, cyber);
     }
 
     private void OnShutdown(EntityUid uid, CyberpsychosisComponent component, ComponentShutdown args)
@@ -35,9 +58,31 @@ public sealed class CyberpsychosisSystem : EntitySystem
         _alerts.ClearAlert(uid, "CyberpsychosisSanity");
     }
 
+    public void RecalculateSanity(EntityUid mob, CyberpsychosisComponent component)
+    {
+        var totalLoad = 0f;
+        var count = 0;
+
+        if (TryComp<ImplantedComponent>(mob, out var implanted))
+        {
+            foreach (var implant in implanted.ImplantContainer.ContainedEntities)
+            {
+                if (!TryComp<CyberpsychosisLoadComponent>(implant, out var load))
+                    continue;
+
+                totalLoad += load.SanityCost;
+                count++;
+            }
+        }
+
+        component.ActiveImplantCount = count;
+        component.SanityValue = Math.Clamp(component.BaseSanity - (int) MathF.Round(totalLoad), 0, component.BaseSanity);
+        RefreshAlert(mob, component);
+    }
+
     public void RefreshAlert(EntityUid uid, CyberpsychosisComponent component)
     {
-        var severity = SeverityFromSanity(component.SanityValue);
+        var severity = SeverityFromSanity(component);
 
         component.CurrentState = (SanityState)(severity - 1);
         Dirty(uid, component);
@@ -45,9 +90,9 @@ public sealed class CyberpsychosisSystem : EntitySystem
         _alerts.ShowAlert(uid, "CyberpsychosisSanity", (short) severity);
     }
 
-    private static int SeverityFromSanity(int sanity)
+    private static int SeverityFromSanity(CyberpsychosisComponent c)
     {
-        return sanity switch
+        return c.SanityValue switch
         {
             > 70 => 1,
             > 45 => 2,
